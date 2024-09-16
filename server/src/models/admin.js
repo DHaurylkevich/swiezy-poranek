73e2bdd3ed81fd4467b5609b9a382a5fe592e281
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-09-16 16:04:57 +0200

    Fix

--- server/src/models/admin.js
+++ server/src/models/admin.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
+
 const AdminSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
--- server/src/services/adminService.js
+++ server/src/services/adminService.js
@@ -1,4 +1,4 @@
-const Admin = require("../models/Admin.js");
+const Admin = require('../models/admin');
 
 const bcrypt = require("bcrypt");
 const jwt = require("../utility/jwt");
