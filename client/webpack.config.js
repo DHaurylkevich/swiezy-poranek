31f9b386ac1e9c48a2fa07370cdefc35f890432d
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-10-13 23:57:17 +0200

    Fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,8 @@ const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -29,7 +31,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ["vue-style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.(js)$/,
@@ -52,7 +54,10 @@ module.exports = {
     },
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin()],
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ],
         splitChunks: {
             chunks: 'all',
         },
@@ -64,10 +69,12 @@ module.exports = {
             favicon: "./public/favicon.ico"
         }),
         new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
         new webpack.DefinePlugin({
             "BASE_URL": JSON.stringify("https://swiezy-poranek.vercel.app" || "http://localhost:8000"),
             "VUE_APP_API_URL": JSON.stringify("https://swiezy-api.vercel.app/api" || "http://localhost:4242/api"),
-            // "VUE_APP_API_URL": JSON.stringify("http://localhost:4242/api"),
             __VUE_OPTIONS_API__: true,
             __VUE_PROD_DEVTOOLS__: false,
             __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
