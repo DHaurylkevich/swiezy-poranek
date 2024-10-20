a02c98b5cdba2cd0f2120e6239526a8a77c4b983
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-10-21 00:52:32 +0300

    CORS fix2.0

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ const app = express();
 connectDB();
 
 const allowedOrigins = [
-    "swiezyporanek.com",
-    "www.swiezyporanek.com",
+    "https://www.swiezyporanek.com/",
     "https://swiezy-poranek.vercel.app",
     "http://localhost:8000",
     "http://192.168.0.186:8000",
